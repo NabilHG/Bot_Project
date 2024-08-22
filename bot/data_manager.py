320e704ddad0d37dc98fe88480d98d21feeb4fd1
NabilHG <nabilhajjoune@gmail.com>
2024-08-22 20:15:57 +0200

    done backtest correctly, good profit

--- bot/data_manager.py
+++ bot/data_manager.py
@@ -5,7 +5,6 @@ from ta.momentum import RSIIndicator
 import yfinance as yf
 import asyncio
 import talib as ta
-
 matrix = {
     "2000": ["MSFT", "GE", "CSCO", "WMT", "XOM", "INTC", "C", "PFE", "NOK", "TM", "DTE", "HD", "ORCL", "MRK"],
     "2005": ["XOM", "GE", "MSFT", "C", "BP", "SHEL", "TM", "WMT", "IBM", "JNJ", "COP", "INTC", "AIG", "PFE"],
@@ -14,9 +13,6 @@ matrix = {
     "2020": ["AAPL", "MSFT", "AMZN", "GOOG", "META", "BRK-B", "TSM", "ASML", "TSLA", "BABA", "JPM", "V", "MA", "UNH", "HD"],
     # "2025": ["MSFT", "AAPL", "NVDA", "GOOG", "AMZN", "META", "BRK-B", "LLY", "AVGO", "TSM", "NVO", "JPM"],
 }
-
-
-
 async def get_date(year):
     year_map = {  
         "2000": ("2000", "2004"),
@@ -26,7 +22,6 @@ async def get_date(year):
         "2020": ("2020", "2024"),
         # "2025": ("2025", "2029"),
     }
-
     if year in year_map:
         year1, year2 = year_map[year]
         start_date = datetime.strptime(f"{year1}-01-01", "%Y-%m-%d")
@@ -36,38 +31,28 @@ async def get_date(year):
         raise ValueError(
             f"Year {year} is not valid. Valid years are: {', '.join(year_map.keys())}"
         )
-
-
 async def trim_data(data, ticker):
     # Inicializar diccionarios para almacenar los datos
     data_rsi = {'Symbol': ticker, 'RSI': {}}
     data_close_price = {'Symbol': ticker, 'CLOSE': {}}
-
     # Iterar sobre cada fila en el DataFrame
     for date, values in data[['Close', 'RSI']].iterrows():
     # for date, values in data[['Close', 'RSI']].iterrows():
-
         # Formatear la fecha como cadena
         date = date.strftime("%Y-%m-%d")
-
         # Almacenar los valores en los diccionarios correspondientes
         data_rsi["RSI"][date] = values['RSI']
         data_close_price["CLOSE"][date] = values['Close']
-
     # Devolver los diccionarios con los datos almacenados
     all_data = {
         "rsi": data_rsi,
         "close_price": data_close_price,
     }
     return all_data
-
-
-
 async def get_data():
     folder_path = "data"
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
-
     for year in matrix:
         year_folder = f"{folder_path}/{year}"
         if not os.path.exists(year_folder):
@@ -76,52 +61,42 @@ async def get_data():
             os.makedirs(f"{year_folder}/rsi")
         if not os.path.exists(f"{year_folder}/close_price"):
             os.makedirs(f"{year_folder}/close_price")
-
         for ticker in matrix[year]:
             start_date, end_date = await get_date(f'{year}')
             start_date = start_date - timedelta(days=200)  # Extraer datos adicionales si es necesario
             start_date = start_date.strftime("%Y-%m-%d")
             end_date = end_date.strftime("%Y-%m-%d")
-
             print(f"Fetching data for {ticker} from {start_date} to {end_date}")
             data = yf.download(ticker, start=start_date, end=end_date)
-            
+        
             # Calcular RSI
             rsi = RSIIndicator(close=data['Close'], window=14)
             data['RSI'] = rsi.rsi()
-            
+        
             # Eliminar filas con NaN después de calcular RSI y MACD
             data = data.dropna()
-            
+        
             # Filtrar los datos al rango de fechas deseado
             all_data = await trim_data(data, ticker)
-
             for folder_name, data_dict in all_data.items():
                 file_path = os.path.join(folder_path, year, folder_name, f"{ticker}.json")
                 if not os.path.exists(file_path):
                     with open(file_path, "w") as json_file:
                         data_dict_str_keys = {str(k): v for k, v in data_dict.items()}
                         json.dump(data_dict_str_keys, json_file, indent=4)
-            
+        
             await asyncio.sleep(2)  # Espera 2 segundos antes de pasar a la siguiente iteración
-
-
-
-
-
 async def update_data():
     print("Here we're going to update data every day")
-
     year = list(matrix.keys())[-1]
     folder_path = "data"
     data_rsi = {}
     data_close_price = {}
-    
+
     '''TODO'''
     #To calculate daily rsi, you must read the previous 30 days of rsis
     #If the is no data becouse we are in a new year, for example, search in the previous year
     # If the file does not exist in the previous year, we have to download the data and calculate
-
     for ticker in matrix[year]:
         print(ticker)
         data = yf.download(ticker, period="1d")
@@ -137,16 +112,12 @@ async def update_data():
         year = year + 1
         year = str(year)
         data_rsi, data_close_price = await trim_data(data, ticker)
-
         if not os.path.exists(f"{folder_path}/{year}/close_price/{ticker}.json"):
             file_path = os.path.join(folder_path, year, "close_price", f"{ticker}.json")
             with open(file_path, "w") as json_file:
                 json.dump(data_close_price, json_file, indent=4)
-
         if not os.path.exists(f"{folder_path}/{year}/rsi/{ticker}.json"):
             file_path = os.path.join(folder_path, year, "rsi", f"{ticker}.json")
             with open(file_path, "w") as json_file:
                 json.dump(data_rsi, json_file, indent=4)
-
-        await asyncio.sleep(2)  # Espera 2 segundos antes de pasar a la siguiente iteración
-        
+        await asyncio.sleep(2)  # Espera 2 segundos antes de pasar a la siguiente iteración 
--- bot/handlers/backtest.py
+++ bot/handlers/backtest.py
@@ -12,8 +12,6 @@ import talib as ta
 
 router = Router()
 
-
-
 async def load_data(base_path, subfolder_name, type):
     data_list = []
     # Definir los rangos de fechas por year_folder
@@ -24,7 +22,6 @@ async def load_data(base_path, subfolder_name, type):
         "2015": ("2015-01-01", "2019-12-30"),
         "2020": ("2020-01-01", (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d"))  # Hasta ayer
     }
-
     # Recorrer todas las subcarpetas del segundo nivel
     for year_folder in os.listdir(base_path):
         year_folder_path = os.path.join(base_path, year_folder)
@@ -42,65 +39,45 @@ async def load_data(base_path, subfolder_name, type):
                             file_path = os.path.join(target_folder_path, filename)
                             with open(file_path, "r") as file:
                                 data = json.load(file)
-                                
+                              
                                 # Filtrar las fechas dentro del rango especificado
                                 filtered = {
                                     date: price for date, price in data[type].items()
                                     if start_date <= date <= end_date
                                 }
-
                                 # Si hay datos filtrados, agregar al array con el símbolo incluido
                                 if filtered:
                                     data_list.append({
                                         "Symbol": data["Symbol"],
                                         type: filtered
                                     })
-
     return data_list
-
-
 async def calculate_month_diff(begin_date):
-
     ancient_date = datetime.strptime(f"{begin_date}", "%Y-%m-%d")
-
     current_date = datetime.now()
-
     delta = relativedelta(current_date, ancient_date)
-
     total_months = delta.years * 12 + delta.months
-
     return total_months
-
-
 async def calculate_maximum_drawdown_profit():
     data_close_price_raw = await load_data("data", "close_price", "CLOSE")
     data_rsi_raw = await load_data("data", "rsi", "RSI")
-
     closing_prices = {}
     for response in data_close_price_raw:
         symbol = response["Symbol"]
         if symbol not in closing_prices:
             closing_prices[symbol] = {}
-
         for date, prices in response["CLOSE"].items():
             closing_prices[symbol][date] = {"close": prices}
-
     rsi_data = {}
     for data in data_rsi_raw:
         symbol = data["Symbol"]
         if symbol not in rsi_data:
             rsi_data[symbol] = {}
-
         for date, rsi in data["RSI"].items():
             rsi_data[symbol][date] = rsi
-
-
     df = await get_dataframe(rsi_data, closing_prices)
     max_drawdown, profitability, average_hold_duration, avg_notification = await simulation(df)
-
     return max_drawdown, profitability, average_hold_duration, avg_notification
-
-
 async def get_dataframe(rsi_data, close_data):
     # Crear un DataFrame para RSI, Close y MACD
     data_tuples = []
@@ -109,21 +86,14 @@ async def get_dataframe(rsi_data, close_data):
             close = float(close_data[empresa][fecha]["close"]) if fecha in close_data[empresa] else None
             data_tuples.append((fecha, empresa, "RSI", rsi))
             data_tuples.append((fecha, empresa, "Close", close))
-
     df = pd.DataFrame(data_tuples, columns=["Fecha", "Empresa", "Tipo", "Valor"])
     df.set_index(["Fecha", "Empresa", "Tipo"], inplace=True)
     df.index = df.index.set_levels(pd.to_datetime(df.index.levels[0]), level=0)
     df = df.unstack(level="Tipo")
     df.sort_index(inplace=True)
-
     # Opcional: Eliminar filas donde todos los valores sean NaN en df
     df_clean = df.dropna(how='all')
-
     return df_clean
-
-
-
-
 async def simulation(df):
     # Inicializar el DataFrame para almacenar la fecha y el valor del portafolio
     df_portfolio_tracking = pd.DataFrame(columns=["Fecha", "Portfolio Value"])
@@ -137,7 +107,6 @@ async def simulation(df):
     buy_notification = 0
     total_percent = 0
     cp_out = 0
-
     # Simular las operaciones
     for date, group in df.groupby(level="Fecha"):
         close_prices = {
@@ -150,8 +119,6 @@ async def simulation(df):
             for ticker in portfolio.keys()
             if (date, ticker) in group.index
         }
-
-
         # Señales de compra y venta
         for ticker in portfolio.keys():
             if (
@@ -170,7 +137,7 @@ async def simulation(df):
                         # Comprar si no hay acciones de este ticker en cartera
                         if portfolio[ticker] == 0:
                             print(f'Compramos {ticker}, rsi: {rsis[ticker]}, fecha: {date}, cash: {cash}')
-                            amount_to_invest = initial_cash * 0.3
+                            amount_to_invest = initial_cash * 0.6
                             if amount_to_invest >= cash:
                                 shares_to_buy = cash / close_prices[ticker]
                                 cash = 0
@@ -183,7 +150,6 @@ async def simulation(df):
                             buy_notification += 1
                             buy_prices[ticker] = close_prices[ticker]
                             buy_dates[ticker] = date
-
                     # Señal de venta: RSI en sobrecompra (>=70) 
                     elif (
                         rsis[ticker] >= 70
@@ -193,15 +159,12 @@ async def simulation(df):
                         cp_out = close_prices[ticker]
                         percent = ((cp_out - buy_prices[ticker]) / buy_prices[ticker]) * 100
                         total_percent += percent
-
                         cash += portfolio[ticker] * close_prices[ticker]
                         print(f'Cash despues de vender: {cash}')
                         portfolio[ticker] = 0
-
                         hold_durations.append((date - buy_dates[ticker]).days)
                         buy_prices[ticker] = None
                         buy_dates[ticker] = None
-
                     # Señal de stop loss: Venta si el precio cae un 10% o más desde la compra
                     elif (
                         buy_prices[ticker] is not None
@@ -211,15 +174,12 @@ async def simulation(df):
                         cp_out = close_prices[ticker]
                         percent = ((cp_out - buy_prices[ticker]) / buy_prices[ticker]) * 100
                         total_percent += percent
-
                         cash += portfolio[ticker] * close_prices[ticker]
                         print(f'Cash despues de vender: {cash}')
                         portfolio[ticker] = 0
-
                         hold_durations.append((date - buy_dates[ticker]).days)
                         buy_prices[ticker] = None
                         buy_dates[ticker] = None
-
         # Valor actual de la cartera en cada paso
         current_value = cash + sum(
             portfolio[ticker] * close_prices[ticker]
@@ -234,11 +194,9 @@ async def simulation(df):
         df_portfolio_tracking = pd.concat(
             [df_portfolio_tracking, temp_df], ignore_index=True
         )
-
     # Calcular duraciones promedio y otros datos finales
     if hold_durations:
         average_hold_duration = sum(hold_durations) / len(hold_durations)
-
     # Calcular max drawdown y rentabilidad
     df_portfolio_tracking["Peak"] = df_portfolio_tracking["Portfolio Value"].cummax()
     df_portfolio_tracking["Drawdown"] = (
@@ -247,14 +205,11 @@ async def simulation(df):
     df_portfolio_tracking["Drawdown Percent"] = (
         df_portfolio_tracking["Drawdown"] / df_portfolio_tracking["Peak"]
     )
-
     max_drawdown = df_portfolio_tracking["Drawdown Percent"].min()
     final_portfolio_value = df_portfolio_tracking["Portfolio Value"].iloc[-1]
     profitability = (final_portfolio_value - initial_cash) / initial_cash * 100
-
     total_months = await calculate_month_diff("2000-01-01")
     avg_notification = buy_notification / total_months
-
     # Redondear y formatear los resultados
     max_drawdown = round(max_drawdown * 100, 2)
     profitability = round(profitability, 2)
@@ -265,20 +220,13 @@ async def simulation(df):
     print(f"Profitability: {profitability}%")
     print(f"Average Hold Duration: {average_hold_duration} days")
     print(f"Average Notifications per Month: {avg_notification}")
-
     return str(max_drawdown), str(profitability), str(average_hold_duration), str(avg_notification)
-
-
-
-
-
 @router.message(Command(commands=["backtest", "BACKTEST", "Backtest", "BackTest"]))
 async def backtest_handler(message: Message):
-
     max_drawdown, profitability, average_hold_duration, avg_notification = await calculate_maximum_drawdown_profit()
-    
+  
     msg = '🧪 BackTest 🧪\n\n' + f'🧮 Media de alertas mensuales: <b>{avg_notification}</b>\n' + f"🔹 Media de días que se mantiene una acción en cartera: <b>{average_hold_duration}</b>\n" + f'☠️ Maximum draw down: <b>{max_drawdown}%</b>\n' + f'💰 Rentabilidad: <b>{profitability}%</b>'
-    
+  
     await message.answer(str(msg), parse_mode='HTML')
-           
+         
     return
